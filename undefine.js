350b144a1b9abd37f2d393c40352015a39498ee9
programmerzobayer <programmerzobayer@gmail.com>
2022-11-25 19:26:15 +0600

    Upload undefineJs

---
+++ undefine.js
@@ -0,0 +1,16 @@
+let Name ;
+console.log(Name);        //variable value not assign 
+
+
+function number (num1,num2){
+    console.log(num1, num2);
+}
+const result = number(2);        //  parameter value not pass
+
+ const undef= undefined ;
+ console.log(undef);       // undefined 
+
+
+ const student = {Names:'jubayer', ID:1, work:'student'}
+ const {Phone} = student ;
+ console.log(Phone);   //propertis name not found
