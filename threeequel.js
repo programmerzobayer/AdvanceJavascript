9f2625758fc5cda2758d03d7e2ff06499a14dd2f
programmerzobayer <programmerzobayer@gmail.com>
2022-11-25 19:41:04 +0600

    threeequelJs

---
+++ threeequel.js
@@ -0,0 +1,19 @@
+const num1 =4 ;
+const num2 = "4" ;
+
+if(num1 == num2){
+    console.log("Number same");
+}
+else{   // == mean  two equel only chack value
+    console.log("Number not same") ;
+}
+
+const num3 =4 ;
+const num4 = "4" ;
+
+if(num3 === num4){
+    console.log("Number same");
+}
+else{   // == mean  two equel chack value and data type
+    console.log("Number not same") ;
+}
