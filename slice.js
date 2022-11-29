109510693685175592fb7d6d56a819233f0c3b73
programmerzobayer <programmerzobayer@gmail.com>
2022-11-29 14:24:41 +0600

    Upload sliceJs

---
+++ slice.js
@@ -0,0 +1,5 @@
+const num = [4,5,6,8,1,3,2,9,45,76]
+const Slicing = num.slice(3 ,8) ; // slice  input start and previous end
+//console.log(Slicing);
+const Splicing = num.splice(2, 3) ; // splice input start and delete total number
+console.log(Splicing);
