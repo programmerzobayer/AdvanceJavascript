25d00a3ac9e0280bf7f69dc6f38dba4da3265d18
programmerzobayer <programmerzobayer@gmail.com>
2022-12-03 19:55:14 +0600

    Upload new.js

---
+++ new.js
@@ -0,0 +1,13 @@
+class Person{
+ constructor(name,profession,salary){
+  this.Name= name ;
+  this.Profession= profession ;
+  this.Salary= salary ;
+ }
+}
+
+const personOne= new Person("rafeh","programmer",2500);
+console.log(personOne);
+
+const personTwo= new Person("jubayer","developer",500);
+console.log(personTwo);
